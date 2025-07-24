207a58975bf8168a1b5ba1671ebf3e6204cdd4d5
kyu3638 <kyu3638@gmail.com>
2025-07-24 18:10:14 +0900

    feat. 블로그, 이력서 네비게이션

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,34 @@
+"use client";
+
 import IconGithub from "@/shared/icons/IconGithub";
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/shared/utils/cn";
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <div className="z-10 flex h-[56px] w-full shrink-0 items-center border-b border-gray-300 px-4">
       <Link href={"/"}>
         <span className="font-fira-mono text-3xl font-bold">KYU</span>
       </Link>
-      <div className="flex grow items-center justify-end gap-4">
+      <div className="font-fira-mono flex grow items-center gap-4 p-16 text-xl">
+        <Link
+          href="/blog/all"
+          className={`${cn(pathname.startsWith("/blog") ? "text-gray-800" : "text-gray-500")} font-medium`}
+        >
+          BLOG
+        </Link>
+        <Link
+          href="/resume"
+          className={`${cn(pathname.startsWith("/resume") ? "text-gray-800" : "text-gray-500")} font-medium`}
+        >
+          RESUME
+        </Link>
+      </div>
+      <div className="flex items-center justify-end gap-4">
         <Link href="https://github.com/kyu3638" target="_blank">
           <IconGithub />
         </Link>
